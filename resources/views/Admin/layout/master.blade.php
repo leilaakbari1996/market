1d8c10118755a3a3e065b667e3765fd09de0a175
leilaakbari1996 <leila.akbari1996@gmail.com>
2021-09-10 18:09:32 +0430

    edit Category

--- resources/views/Admin/layout/master.blade.php
+++ resources/views/Admin/layout/master.blade.php
@@ -219,7 +219,18 @@
     <!-- /.sidebar -->
   </aside>
 
-  @include('Admin.layout.success')
+  @if (session()->has('success'))
+  <div class="row">
+    <div class="col-sm-2"></div>
+        <div class="col-sm-10">
+            <div class="alert alert-success alert-dismissible mr-3">
+                <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+                <h5><i class="icon fa fa-check"></i> توجه!</h5>
+                {{session()->get('success')}}
+            </div>
+        </div>
+    </div>
+  @endif
   @yield('content')
   <footer class="main-footer">
     <strong>CopyLeft &copy; 2018 <a href="http://github.com/leilaakbari1996/">لیلا اکبری</a>.</strong>
